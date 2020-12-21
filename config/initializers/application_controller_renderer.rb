088fb38470a61902952c20153d8bd953a23e3ba1
e-last <statw54@gmail.com>
2020-12-21 07:06:59 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cc57a076e456eeb973574683cc2fe8256d116723d34f1a1d5f03edb45b7dcfaab7be48960e1078bdcf75ab7853b014fb51d5742f87c442277b87225f4e5171c6'
